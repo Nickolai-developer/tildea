9062be43a7164ca14916efa944ccb822915a8ed7
Nickolai-dev <n.solenii2016@gmail.com>
2023-12-15 12:20:17 +0400

    add more tests for either type repr;

--- src/tests/type-repr.ts
+++ src/tests/type-repr.ts
@@ -2,6 +2,9 @@ import {
     NullableOptions,
     ReprOptions,
     ScalarDefinition,
+    TildaArrayType,
+    TildaEitherType,
+    TildaStaticArray,
 } from "../interfaces.js";
 import { ReprDefinitions, typeRepr } from "../repr.js";
 import TildaScalarType from "../tilda-scalar-type.js";
@@ -182,6 +185,276 @@ const unitTest: UnitTest = {
                 ReprDefinitions.UNDEFINED +
                 ReprDefinitions.DELIM_OR +
                 ReprDefinitions.NO_PROPERTY,
+        );
+
+        const arrType0: TildaArrayType = {
+            _tildaEntityType: "array",
+            elemDefinition: {
+                type: Int,
+                ...null0,
+            },
+        };
+        const arrType1: TildaArrayType = {
+            _tildaEntityType: "array",
+            elemDefinition: {
+                type: Int,
+                defined: false,
+                nullable: false,
+                optional: false,
+            },
+        };
+        const arrType2: TildaArrayType = {
+            _tildaEntityType: "array",
+            elemDefinition: {
+                type: Int,
+                defined: false,
+                nullable: true,
+                optional: true,
+            },
+        };
+        clock.assertEqual(
+            typeRepr({ type: arrType0, ...null0 }, { hasPropertyCheck: true }),
+            "Int[]",
+        );
+        clock.assertEqual(
+            typeRepr(
+                {
+                    type: arrType0,
+                    defined: true,
+                    nullable: true,
+                    optional: false,
+                },
+                { hasPropertyCheck: true },
+            ),
+            "Int[]" + ReprDefinitions.DELIM_OR + ReprDefinitions.NULL,
+        );
+        clock.assertEqual(
+            typeRepr(
+                {
+                    type: arrType1,
+                    defined: true,
+                    nullable: true,
+                    optional: false,
+                },
+                { hasPropertyCheck: true },
+            ),
+            "(Int" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.UNDEFINED +
+                ")[]" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL,
+        );
+        clock.assertEqual(
+            typeRepr(
+                {
+                    type: arrType2,
+                    defined: true,
+                    nullable: true,
+                    optional: false,
+                },
+                { hasPropertyCheck: false },
+            ),
+            "(Int" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.UNDEFINED +
+                ")[]" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL,
+        );
+        clock.assertEqual(
+            typeRepr(
+                {
+                    type: arrType2,
+                    defined: true,
+                    nullable: true,
+                    optional: false,
+                },
+                { hasPropertyCheck: true },
+            ),
+            "(Int" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.UNDEFINED +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NO_PROPERTY +
+                ")[]" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL,
+        );
+
+        const sArray0: TildaStaticArray = {
+            _tildaEntityType: "staticArray",
+            types: [
+                { type: Int, ...null0 },
+                { type: String_, ...null0 },
+                { type: Int, defined: true, nullable: true, optional: true },
+            ],
+        };
+        clock.assertEqual(
+            typeRepr(
+                {
+                    type: { ...sArray0, name: "StaticArrayProperties" },
+                    defined: true,
+                    nullable: true,
+                    optional: false,
+                },
+                { hasPropertyCheck: false },
+            ),
+            "StaticArrayProperties" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL,
+        );
+        clock.assertEqual(
+            typeRepr(
+                {
+                    type: sArray0,
+                    defined: true,
+                    nullable: true,
+                    optional: false,
+                },
+                { hasPropertyCheck: false },
+            ),
+            "[Int" +
+                ReprDefinitions.DELIM_COLON +
+                "string" +
+                ReprDefinitions.DELIM_COLON +
+                "Int" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL +
+                "]" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL,
+        );
+        clock.assertEqual(
+            typeRepr(
+                {
+                    type: sArray0,
+                    defined: true,
+                    nullable: true,
+                    optional: false,
+                },
+                { hasPropertyCheck: true },
+            ),
+            "[Int" +
+                ReprDefinitions.DELIM_COLON +
+                "string" +
+                ReprDefinitions.DELIM_COLON +
+                "Int" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NO_PROPERTY +
+                "]" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL,
+        );
+
+        const e0: TildaEitherType = {
+            _tildaEntityType: "either",
+            types: [
+                {
+                    type: Int,
+                    ...null0,
+                },
+                {
+                    type: String_,
+                    ...null0,
+                },
+            ],
+        };
+        const e1: TildaEitherType = {
+            _tildaEntityType: "either",
+            types: [
+                {
+                    type: Int,
+                    defined: false,
+                    nullable: true,
+                    optional: false,
+                },
+                {
+                    type: String_,
+                    defined: false,
+                    nullable: true,
+                    optional: false,
+                },
+            ],
+        };
+        clock.assertEqual(
+            typeRepr(
+                {
+                    type: { ...e0, name: "EitherType1" },
+                    defined: true,
+                    nullable: true,
+                    optional: false,
+                },
+                { hasPropertyCheck: false },
+            ),
+            "(EitherType1" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL +
+                ")",
+        );
+        clock.assertEqual(
+            typeRepr(
+                {
+                    type: { ...e1, name: "EitherType1" },
+                    defined: true,
+                    nullable: true,
+                    optional: false,
+                },
+                { hasPropertyCheck: false },
+            ),
+            "(EitherType1" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.UNDEFINED +
+                ")",
+        );
+        clock.assertEqual(
+            typeRepr(
+                {
+                    type: {
+                        _tildaEntityType: "either",
+                        types: [
+                            {
+                                type: Int,
+                                defined: false,
+                                nullable: false,
+                                optional: false,
+                            },
+                            {
+                                type: String_,
+                                defined: true,
+                                nullable: false,
+                                optional: false,
+                            },
+                            {
+                                type: e0,
+                                defined: true,
+                                nullable: false,
+                                optional: false,
+                            },
+                        ],
+                    },
+                    defined: false,
+                    nullable: true,
+                    optional: false,
+                },
+                { hasPropertyCheck: false },
+            ),
+            "(Int" +
+                ReprDefinitions.DELIM_OR +
+                "string" +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.NULL +
+                ReprDefinitions.DELIM_OR +
+                ReprDefinitions.UNDEFINED +
+                ")",
         );
     },
 };
