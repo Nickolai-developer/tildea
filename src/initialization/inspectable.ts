d7aae8fd4b423da0b3870f4156fa1c160397689a
Nickolai-dev <n.solenii2016@gmail.com>
2024-01-09 06:12:22 +0400

    move validateSchema;

--- src/initialization/inspectable.ts
+++ src/initialization/inspectable.ts
@@ -1,9 +1,59 @@
-import { useOptions, usedReprOpts } from "../config.js";
 import type { Schema } from "../entities/schema.js";
+import type {
+    PropertyValidationResult,
+    ReprOptions,
+    SchemaValidationResult,
+} from "../index.js";
+import { useOptions, usedReprOpts } from "../config.js";
 import { TildaRuntimeError } from "../errors.js";
-import { type ReprOptions, type SchemaValidationResult } from "../index.js";
-import { validateSchema } from "../validation/validate-schema.js";
 import { TypeDescription } from "./schema-builder.js";
+
+function validateSchema(obj: object, schema: Schema): SchemaValidationResult {
+    const errors: PropertyValidationResult[] = [];
+
+    const stack: PropertyValidationResult[][] = [];
+    let currentDepth = 0;
+    let currentFacility = errors;
+
+    const pickFacility = (newDepth: number): void => {
+        if (newDepth === currentDepth) {
+            return;
+        }
+        if (newDepth > currentDepth) {
+            currentDepth++;
+            stack.push(currentFacility);
+            const last = currentFacility[currentFacility.length - 1];
+            if (!last.subproperties) {
+                last.subproperties = [];
+            }
+            currentFacility = last.subproperties!;
+        } else {
+            currentDepth--;
+            currentFacility = stack.pop()!;
+        }
+        pickFacility(newDepth);
+    };
+
+    for (const { depth, ...result } of schema.execute({
+        obj: { key: obj },
+        key: "key",
+        currentDepth: 0,
+        depMap: {},
+    })) {
+        pickFacility(depth);
+        currentFacility.push(result);
+    }
+
+    if (errors.length) {
+        if (errors[0].subproperties) {
+            return { errors: errors[0].subproperties! };
+        } else {
+            return { errors: [{ ...errors[0], name: "" }] };
+        }
+    }
+
+    return { errors: null };
+}
 
 export const Store = new WeakMap<Function, Schema>();
 
